c53519c833fe76eb5b4513cd554a1cc80c493ef6
A113E <amg11amg2@gmail.com>
2025-12-25 18:02:45 -0500

    Campos date, datetime avanzado con computados y validaciones

--- models/producto.py
+++ models/producto.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
 from odoo.exceptions import ValidationError
+from odoo import _
 import re # Para expresiones regulares
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
 
 # Modelo
 class Producto(models.Model):
@@ -111,6 +114,95 @@ class Producto(models.Model):
     help='El producto incluye garantía del fabricante'
   )
 
+  # Campos Date (Avanzado)
+  fecha_fabricacion = fields.Date(
+    string='Fecha de fabricación',
+    help='Fecha en la que se fabricó el producto',
+    copy=False, # no se copia el valor del campo al duplicar
+    tracking=True, # para trazabilidad
+  )
+
+  fecha_vencimiento = fields.Date(
+    string='Fecha de vencimiento',
+    help='Fecha después de la cual el producto no puede ser vendido/consumido',
+    copy=False, # no se copia el valor del campo al duplicar
+    tracking=True # chatter
+  )
+
+  fecha_ultima_revision = fields.Date(
+    string='Fecha de última revisión',
+    help='Fecha de la última revisión de calidad',
+    copy=False, # no se copia el valor del campo al duplicar
+    tracking=True # chatter
+  )
+
+  fecha_proxima_revision = fields.Date(
+    string='Fecha de próxima revisión',
+    help='Fecha estimada de la próxima revisión',
+    copy=False, # no se copia el valor del campo al duplicar
+    compute='_compute_fecha_proxima_revision',
+    store=True # Se guarda en la Bd
+  )
+
+  # Campos Datetime (Avanzado)
+  fecha_hora_recepcion = fields.Datetime(
+    string='Fecha/hora de recepción',
+    help='Fecha y hora exacta en el que el producto fue recibido en el almacén',
+    default= lambda self: fields.Datetime.now(),
+  )
+
+  fecha_hora_ultima_venta = fields.Datetime(
+    string='Fecha/hora última venta',
+    help='Fecha y hora de la última venta registrada de este producto',
+    copy=False, # no se copia el valor del campo al duplicar
+    readonly=True # solo lectura
+  )
+
+  fecha_hora_alerta_stock = fields.Datetime(
+    string='Fecha/hora Alerta Stock',
+    help='Fecha y hora en que se debe alertar por stock bajo',
+    copy=False, # no se copia el valor del campo al duplicar
+    store=True, # se guarda en la bd
+    compute='_compute_fecha_alerta_stock'
+  )
+
+  # Campos Calculados temporales (volatiles) No se guardan en bd
+  dias_para_vencer = fields.Integer(
+    string='Dias para vencer',
+    help='Número de dias restantes para la fecha de vencimiento',
+    store=True,
+    compute='_compute_dias_para_vencer',
+    tracking=True # trazabilidad
+  )
+
+  antiguedad_dias = fields.Integer(
+    string='Días de antiguedad',
+    help='Número de días desde la fabricación',
+    store=False,
+    compute='_compute_antiguedad'
+  )
+
+  esta_por_vencer = fields.Boolean(
+    string='¿Está por vencer?',
+    help='Indica si el producto está próximo a vencer (< 30 dias)',
+    store=True, # si se guarda en la bd para notificar
+    compute='_compute_esta_por_vencer'
+  )
+
+  necesita_revision = fields.Boolean(
+    string='¿Necesita revisión?',
+    help='Indica si el producto necesita revisión de calidad',
+    store=True, # se guarda en bd para notificar
+    compute='_compute_necesita_revision'
+  )
+
+  alerta_stock_bajo = fields.Boolean(
+    string='Alerta Stock Bajo',
+    help='Indica si el stock es bajo y necesita atención',
+    compute='_compute_alerta_stock_bajo',
+    store=True # se guarda en bd para notificar
+  )
+
   # Selection * Avanzado *
   estado = fields.Selection(
     selection = [
@@ -162,7 +254,7 @@ class Producto(models.Model):
   # Depende del campo refrigeracion
   temperatura_refrigeracion = fields.Float(
     string='Temperatura Refrigeración (°C)',
-    digits=('3,1'), # 3 digitos en total : 18.5
+    digits=(3,1), # 3 digitos en total : 18.5
     help='Temperatura óptima de refrigeración',
     copy=False # no copiar el valor del campo al duplicar
   )
@@ -278,9 +370,9 @@ class Producto(models.Model):
         )
       # Validar que sean lógicos
       if record.precio_venta > 0 and record.precio_compra > record.precio_venta:
-        # Advertencia
-        # En producción puede ser error
-        pass # Genera un log aquí
+        raise ValidationError(
+          _('El precio de compra no puede ser mayor al precio de venta')
+        )
     
   # Validar estado y precio_venta
   @api.constrains('estado','precio_venta')
@@ -323,6 +415,173 @@ class Producto(models.Model):
             _('La temperatura de refrigeracion debe estar entre -50 y 10°C')
           )
   
+  # Validaciones temporales
+  # Verifica que la fecha de fabricacion sea mas antigua que la fecha de vencimiento
+  @api.constrains('fecha_fabricacion','fecha_vencimiento')
+  def _check_fechas_logicas(self):
+    """Valida que fecha de fabricacion sea menor que fecha de vencimiento"""
+    for record in self:
+      if record.fecha_fabricacion and record.fecha_vencimiento:
+        if record.fecha_fabricacion > record.fecha_vencimiento:
+          raise ValidationError(
+            _('La fecha de fabriación no puede ser posterior a la fecha de vencimiento')
+          )
+  
+  # Verifica que la fecha de fabricacion no sea en el futuro
+  @api.constrains('fecha_fabricacion')
+  def _check_fecha_fabricacion_pasado(self):
+    """Valida que la fecha de fabricacion no sea en el futuro"""
+    hoy = fields.Date.today()
+
+    for record in self:
+      if record.fecha_fabricacion:
+        if record.fecha_fabricacion > hoy:
+          raise ValidationError(
+            _('La fecha de fabricación no puede ser futurista')
+          )
+  
+  # Verifica que la fecha de vencimiento sea en el futuro
+  @api.constrains('fecha_vencimiento')
+  def _check_fecha_vencimiento_futuro(self):
+    """Validar que la fecha de vencimiento sea en el futuro"""
+    hoy = fields.Date.today()
+
+    for record in self:
+      if record.fecha_vencimiento <= hoy:
+          raise ValidationError(
+            _('La fecha de vencimiento no puede ser en el pasado')
+          )
+  
+  # Verifica que la fecha de revision no sea en el futuro
+  @api.constrains('fecha_ultima_revision')
+  def _check_fecha_revision_pasado(self):
+    """Validar que la fecha de la ultima revision en el futuro"""
+    hoy = fields.Date.today()
+
+    for record in self:
+      if record.fecha_ultima_revision:
+        if record.fecha_ultima_revision > hoy:
+          raise ValidationError(
+            _('La fecha de la última revisión no puede ser futurista')
+          )
+  
+  # Computados para campos temporales
+  # Calcula los dias que quedan para que venza
+  @api.depends('fecha_vencimiento')
+  def _compute_dias_para_vencer(self):
+    """Calcula dias restantes para vencimiento"""
+    hoy = fields.Date.today()
+
+    for record in self:
+      if record.fecha_vencimiento:
+        delta = (record.fecha_vencimiento - hoy).days # Delta calculo y toma los dias solo
+        record.dias_para_vencer = delta if delta > 0 else 0 # El campo toma el valor delta y 0 si es mayor que 0
+      else:
+        record.dias_para_vencer = 0
+  
+  # Calcula los dias de antiguedad segun la fecha de fabricación
+  @api.depends('fecha_fabricacion')
+  def _compute_antiguedad(self):
+    """Calcular los dias de antiguedad"""
+    hoy = fields.Date.today()
+
+    for record in self:
+      if record.fecha_fabricacion:
+        delta = (record.fecha_fabricacion - hoy).days # Delta calculo y toma los dias solo
+        record.antiguedad_dias = delta if delta > 0 else 0
+      else:
+        record.antiguedad_dias = 0
+  
+  # Determina si está cerca a vencer
+  @api.depends('dias_para_vencer')
+  def _compute_esta_por_vencer(self):
+    """Determina si el producto esta proximo a vecer (<- 30 dias)"""
+    for record in self:
+      record.esta_por_vencer = (0 < record.dias_para_vencer <= 30)
+
+  # Determina si necesita revisión
+  @api.depends('fecha_ultima_revision', 'fecha_proxima_revision')
+  def _compute_necesita_revision(self):
+    """Determina si necesita revisión"""
+    hoy = fields.Date.today()
+
+    for record in self:
+      if record.fecha_proxima_revision:
+        record.necesita_revision = (hoy >= record.fecha_proxima_revision)
+      else:
+        record.necesita_revision = False
+  
+  # Calcular fecha de próxima revisión
+  @api.depends('fecha_ultima_revision')
+  def _compute_fecha_proxima_revision(self):
+    """Calcular fecha de próxima revisión (6 meses después de la última)"""
+    for record in self:
+      if record.fecha_ultima_revision:
+        # Sumar 6 meses a la última revisión
+        ultima = fields.Date.from_string(record.fecha_ultima_revision)
+        # Importa relativedelta para el manejo de meses
+        proxima = ultima + relativedelta(months=6) # usando la libreria toma los meses de la última
+        record.fecha_proxima_revision = fields.Date.to_string(proxima)
+      else:
+        record.fecha_proxima_revision = False
+  
+  # Calcular la fecha de alerta de stock bajo
+  @api.depends('cantidad_stock', 'fecha_hora_recepcion')
+  def _compute_fecha_alerta_stock(self):
+    """Calcular la fecha de alerta de stock bajo (30 dias despues de la recepcion si stock es < 5)"""
+    for record in self:
+      if record.cantidad_stock <= 5 and record.fecha_hora_recepcion:
+        recepcion = fields.Datetime.from_string(record.fecha_hora_recepcion)
+        alerta = recepcion + relativedelta(days=30)
+        record.fecha_hora_alerta_stock = fields.Datetime.to_string(alerta)
+      else:
+        record.fecha_hora_alerta_stock = False
+  
+  # Determina si hay alerta de stock bajo
+  @api.depends('cantidad_stock', 'fecha_hora_recepcion', 'fecha_hora_alerta_stock')
+  def _compute_alerta_stock_bajo(self):
+    """Determina si hay alerta de stock bajo"""
+    ahora = fields.Datetime.now() 
+
+    for record in self:
+        # Stock bajo Y (sin fecha de recepción O ya pasaron 30 días)
+        if record.cantidad_stock <= 5:
+            if not record.fecha_hora_recepcion:
+                # Si no hay fecha de recepción pasa la alerta inmediata
+                record.alerta_stock_bajo = True
+            elif record.fecha_hora_alerta_stock:
+                alerta = fields.Datetime.from_string(record.fecha_hora_alerta_stock)
+                record.alerta_stock_bajo = (ahora >= alerta)
+            else:
+                record.alerta_stock_bajo = False
+        else:
+            record.alerta_stock_bajo = False
+  
+  # Métodos Onchange (Cambios en tiempos real)
+  # Reset duracion si no tiene garantia
+  @api.onchange('tiene_garantia')
+  def _onchange_tiene_garantia(self):
+    """Resetear duración de garantía si se desactiva garantia"""
+    for record in self:
+      if not record.tiene_garantia:
+        record.duracion_garantia_mes = 0
+  
+  # Reset temperatura si no requiere refrigeracion
+  @api.onchange('requiere_refrigeracion')
+  def _onchange_requiere_refrigeracion(self):
+    """Resetear temperatura si no requiere refrigeracion"""
+    for record in self:
+      if not record.requiere_refrigeracion:
+        record.temperatura_refrigeracion = 0.0
+
+  # Resetear instrucciones si no es fragil
+  @api.onchange('es_fragil')
+  def _onchange_es_fragil(self):
+    """Resetear instrucciones si no es fragil"""
+    for record in self:
+      if not record.es_fragil:
+        record.instrucciones_fragilidad = False
+
   # Métodos de Acción básico
   # No llevan _, son pensados para botones xml etc.
   # Método write (editar) para activar el producto
@@ -474,6 +733,103 @@ class Producto(models.Model):
             }
         }
   
+  # Métodos de útilidad temporal
+  # Registrar última revisión
+  def action_registrar_revision(self):
+    """Registrar una fecha para la fecha de revisión"""
+    hoy = fields.Date.today()
+
+    for record in self:
+      record.write({
+        'fecha_ultima_revision': hoy,
+        'necesita_revision': False, 
+      })
+    
+    return {
+      'type': 'ir.actions.client',
+      'tag': 'display_notification',
+      'params': {
+        'title': 'Revisión registrada',
+        'message': f'Revisión registrada para {self.name}',
+        'type': 'success',
+        'sticky': False
+      }
+    }
+  
+  # Posponer revisión
+  def action_posponer_revision(self, dias=30):
+    """Posponer una revisión"""
+    hoy = fields.Date.today()
+
+    for record in self:
+      nueva_fecha = hoy + timedelta(days=dias)
+      record.write({
+        'fecha_proxima_revision': nueva_fecha,
+        'necesita_revision': False
+      })
+    
+    return {
+      'type': 'ir.actions.client',
+      'tag': 'display_notification',
+      'params': {
+        'title': 'Revisión pospuesta',
+        'message': f'Próxima revisión pospuesta {dias} dias para {self.name}',
+        'type': 'warning',
+        'sticky': False
+      }
+    }
+  
+  # Actualizar última venta
+  def action_actualizar_ultima_venta(self):
+    """Actualizar la fecha hora de la ultima venta (ESTO ES PARA MODELO VENTAS)"""
+    ahora = fields.Datetime.now()
+
+    for record in self:
+      record.write({
+        'fecha_hora_ultima_venta': ahora,
+      })
+    
+    return True
+  
+  # Calcular rotación
+  def action_calcular_rotacion(self):
+    """Calcular la rotación del inventario (VENTAS)"""
+    pass # Integrar luego con Ventas
+
+  # Métodos para reportes y filtros
+  # Obtener productos activos proximos a vencer
+  def obtener_productos_por_vencer(self, dias=30):
+    """Retorna productos activos con stock que están próximos a vencer"""
+    hoy = fields.Date.today()
+    limite = hoy + timedelta(days=dias)
+
+    return self.search([
+      ('fecha_vencimiento', '!=', False),
+      ('fecha_vencimiento', '<=', limite),
+      ('fecha_vencimiento', '>=', hoy),
+      ('cantidad_stock', '>', 0),
+      ('esta_activo', '=', True)
+    ])
+  
+  # Obtener productos vencidos con stock
+  def obtener_productos_vencidos(self):
+    """Retorna productos vencidos"""
+    hoy = fields.Date.today()
+
+    return self.search([
+      ('fecha_vencimiento', '!=', False),
+      ('fecha_vencimiento', '<', hoy),
+      ('cantidad_stock', '>', 0)
+    ])
+  
+  # Obtener productos activos sin revisión
+  def obtener_productos_sin_revision(self):
+    """Retorna productos activos sin revisión"""
+    return self.search([
+      ('necesita_revision', '=', True),
+      ('esta_activo', '=', True)
+    ])
+  
   # Métodos computados
   @api.depends('estado')
   def _compute_color_estado(self):
@@ -488,30 +844,5 @@ class Producto(models.Model):
 
     for record in self:
       record.color_estado = colores.get(record.estado, 0) # Gris por defecto
-  
-  # Métodos Onchange (Cambios en tiempos real)
-  # Reset duracion si no tiene garantia
-  @api.onchange('tiene_garantia')
-  def _onchange_tiene_garantia(self):
-    """Resetear duración de garantía si se desactiva garantia"""
-    for record in self:
-      if not record.tiene_garantia:
-        record.duracion_garantia_mes = 0
-  
-  # Reset temperatura si no requiere refrigeracion
-  @api.onchange('requiere_refrigeracion')
-  def _onchange_requiere_refrigeracion(self):
-    """Resetear temperatura si no requiere refrigeracion"""
-    for record in self:
-      if not record.requiere_refrigeracion:
-        record.temperatura_refrigeracion = 0.0
-
-  # Resetear instrucciones si no es fragil
-  @api.onchange('es_fragil')
-  def _onchange_es_fragil(self):
-    """Resetear instrucciones si no es fragil"""
-    for record in self:
-      if not record.es_fragil:
-        record.instrucciones_fragilidad = False
         
 
